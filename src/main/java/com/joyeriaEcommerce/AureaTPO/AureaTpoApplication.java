bc46f90a1191e783cf9b92cec38f7fb99df475fe
BlancoUADE <agustin.blanco.vocos@gmail.com>
2026-08-30 19:55:53 -0300

    Setup de Spring Initializr

---
+++ src/main/java/com/joyeriaEcommerce/AureaTPO/AureaTpoApplication.java
@@ -0,0 +1,13 @@
+package com.joyeriaEcommerce.AureaTPO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AureaTpoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AureaTpoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joyeriaEcommerce/AureaTPO/AureaTpoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joyeriaEcommerce.AureaTPO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AureaTpoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
