9e104287269da6b5ee876b262511bb7a2679d958
Erik Berns <erik.christian.berns@gmail.com>
2026-09-10 17:25:05 -0300

    Repositories

---
+++ src/main/java/com/joyeriaEcommerce/AureaTPO/productos/datos/ProductImageRepository.java
@@ -0,0 +1,8 @@
+package com.joyeriaEcommerce.AureaTPO.productos.datos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductImageRepository extends JpaRepository<ProductImage, Long> {
+}
---
+++ src/main/java/com/joyeriaEcommerce/AureaTPO/productos/datos/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.joyeriaEcommerce.AureaTPO.productos.datos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
